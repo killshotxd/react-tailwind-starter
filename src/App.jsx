ea7e74c612351bcd646dd54d9a1e7b5058a7a065
killshotxd <hassanansari211@gmail.com>
2022-10-27 09:07:08 +0530

    Tailwind css integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="flex justify-center items-center text-2xl text-teal-400">
+      App
+    </div>
+  );
 };
 
 export default App;
